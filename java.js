4a1da742fd0f5892901b377be77e0fe8e2ab63a0
eseag <agadagbaesefaith@gmail.com>
2022-06-25 13:26:01 +0100

    first commit

---
+++ java.js
@@ -0,0 +1,5 @@
+const myName = 'ESE'
+const myHeight = '5.10'
+const myCountry = 'Nigeria'
+
+console .log = ('my name is $ {my name} and i am $ {5.10} tall and my country of origin is $ {my country}')
